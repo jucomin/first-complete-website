c8a4fdef77e9bc397bc563cfed35a1550dd38471
Juliana Comin <ju.comin@gmail.com>
2019-10-26 15:00:57 +0200

    Add header and footer :ant:

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div>-->
+    <the-header />
     <router-view />
   </div>
 </template>
-
+<script>
+import TheHeader from './components/TheHeader'
+export default {
+  components: {
+    TheHeader,
+  },
+}
+</script>
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <router-link to="/">
+      <img src="logo.png" alt="Cupcakes & cookies" />
+    </router-link>
+    <ul>
+      <router-link to="/">Home</router-link>
+      <li class="nav-icon">
+        <router-link to="/">
+          <img src="icons/house.png" alt="home" />
+        </router-link>
+      </li>
+      <li class="nav-icon">
+        <router-link to="/">
+          <img src="icons/compass-arrow.png" alt="direct message" />
+        </router-link>
+      </li>
+      <li class="nav-icon">
+        <router-link to="/">
+          <img src="icons/file.png" alt="file" />
+        </router-link>
+      </li>
+      <li class="nav-icon">
+        <router-link to="/">
+          <img src="icons/question-mark.png" alt="questions" />
+        </router-link>
+      </li>
+      <li class="nav-icon">
+        <router-link to="/">
+          <img src="icons/person.png" alt="profile" />
+        </router-link>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+}
+</script>
+
+<style>
+</style>
