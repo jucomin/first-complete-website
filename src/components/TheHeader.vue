b6bbdefc36a0209d304325b144ced641c8bc4b7b
Juliana Comin <ju.comin@gmail.com>
2019-10-26 16:14:55 +0200

    Moving stuffs, change size

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,33 +1,33 @@
 <template>
   <header>
     <router-link to="/">
-      <img src="logo.png" alt="Cupcakes & cookies" />
+      <img src="../assets/logo.png" alt="Cupcakes & cookies" />
     </router-link>
     <ul>
       <router-link to="/">Home</router-link>
       <li class="nav-icon">
         <router-link to="/">
-          <img src="icons/house.png" alt="home" />
+          <img src="../assets/icons/house.png" alt="home" />
         </router-link>
       </li>
       <li class="nav-icon">
         <router-link to="/">
-          <img src="icons/compass-arrow.png" alt="direct message" />
+          <img src="../assets/icons/compass-arrow.png" alt="direct message" />
         </router-link>
       </li>
       <li class="nav-icon">
         <router-link to="/">
-          <img src="icons/file.png" alt="file" />
+          <img src="../assets/icons/file.png" alt="file" />
         </router-link>
       </li>
       <li class="nav-icon">
         <router-link to="/">
-          <img src="icons/question-mark.png" alt="questions" />
+          <img src="../assets/icons/question-mark.png" alt="questions" />
         </router-link>
       </li>
       <li class="nav-icon">
         <router-link to="/">
-          <img src="icons/person.png" alt="profile" />
+          <img src="../assets/icons/person.png" alt="profile" />
         </router-link>
       </li>
     </ul>
@@ -41,4 +41,13 @@ export default {
 </script>
 
 <style>
+.nav-icon {
+  background-color: deeppink;
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  list-style: none;
+  display: inline-block;
+  text-align: center;
+}
 </style>
