4d260b1c3fbd675d2b76f9a843ec79846155d62d
Juliana Comin <ju.comin@gmail.com>
2019-11-20 21:14:24 +0100

    Update fonts

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,53 @@ export default {
 </script>
 
 <style lang="scss">
+@font-face {
+  font-family: 'SignikaLight';
+  font-style: normal;
+  font-weight: 300;
+  src: local('Signika-Light'),
+    url(http://themes.googleusercontent.com/static/fonts/signika/v3/koFYE26bhT88HgRA8f4vbRsxEYwM7FgeyaSgU71cLG0.woff)
+      format('woff');
+}
+
+@font-face {
+  font-family: 'Signika';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Signika'), local('Signika-Regular'),
+    url(http://themes.googleusercontent.com/static/fonts/signika/v3/_M5k9_BZTkfaM2dnN0vNGw.woff)
+      format('woff');
+}
+
+@font-face {
+  font-family: 'SignikaSemibold';
+  font-style: normal;
+  font-weight: 600;
+  src: local('Signika-Semibold'),
+    url(http://themes.googleusercontent.com/static/fonts/signika/v3/FZa_MODmDkR9DTu5jHSnSRsxEYwM7FgeyaSgU71cLG0.woff)
+      format('woff');
+}
+
+@font-face {
+  font-family: 'SignikaBold';
+  font-style: normal;
+  font-weight: 700;
+  src: local('Signika-Bold'),
+    url(http://themes.googleusercontent.com/static/fonts/signika/v3/7M5kxD4eGxuhgFaIk95pBRsxEYwM7FgeyaSgU71cLG0.woff)
+      format('woff');
+}
+
+body,
+select,
+input,
+textarea,
+p,
+a,
+label {
+  color: #444;
+  font-family: Signika, Arial, serif;
+}
+
 body {
   background: #f6c5d4 url(assets/background.jpg) 0 0 repeat;
 }
